16d540d6243a77fbc00c8e1e85109d0e90d47202
cavin18 <cavin.2ankara@gmail.com>
2025-12-19 20:34:31 +0700

    Add files via upload

---
+++ bot2.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from config import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='/', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah login sebagai {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(client.command_prefix):
+        await client.process_commands(message)
+    else:
+        await message.channel.send(message.content)
+
+@client.command()
+async def about(ctx):
+    await ctx.send('Ini adalah echo-bot yang dibuat dengan pustaka discord.py!')
+
+client.run(token)
